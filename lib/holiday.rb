21e3508b732683f04aa8ccb6e5e3ce4bb87e06a2
Ron Sala <ron.sala@gmail.com>
2018-01-03 04:17:43 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -79,7 +79,7 @@ def all_supplies_in_holidays(holiday_hash)
     season_str = season_name.capitalize!
     puts "#{season_str}:"
     season_hash.each do |holiday_name, supply_array|
-      holiday_ary = holiday_name.split(/_/)
+      holiday_ary = holiday_name.split('_')
         holiday_ary.each do |word|
           cap_holiday_ary << word.capitalize!
         holiday_str = cap_holiday_ary.join(" ")
